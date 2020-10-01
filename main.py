4113639520baa4d7719faab966056aed390170d6
daniel <duque1daniel@gmail.com>
2020-10-01 14:38:05 -0700

    fixed spelling issues & added functions)

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def get_file_lines(filename):
+def get_file_lines(filename) :
     return
+
+def lines_printed(backwards) :
+    print()
+
+def lines_printed_random(line_list) :
+    print()
+
+def line_printed_custom(lines_list) :
+    print()
     
