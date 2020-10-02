d17b8da5d4e14717231f3f565f28cee013f9a269
daniel <duque1daniel@gmail.com>
2020-10-01 17:55:26 -0700

    added backwards & random functions

---
+++ README.md
@@ -0,0 +1,5 @@
+PyYeezy Slam:
+This program will print a backwards, random, and custom Yeezy (Kanye West) poem!
+
+Source of poem:
+Source: https://www.reddit.com/r/Poems/comments/9gmld3/kanye_west/
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-def get_file_lines(filename) :
-    return
+import random
+# returns a list of strings containing the lines of the file
+def get_file_lines(filename):
+    lines = open(filename, "r")
+    lines_list = lines.readlines()
+    return lines_list
+# prints the lines of the poem in reverse.
+def lines_printed_backwards(lines_list):
+    number_list = range(len(lines_list))
+    number_list = list(number_list)
+    for num in reversed(number_list):
+        print(f"{number_list[num]} {lines_list[num]}")
 
-def lines_printed(backwards) :
-    print()
-
-def lines_printed_random(line_list) :
-    print()
-
-def line_printed_custom(lines_list) :
-    print()
-    
+def lines_list_printed_random(lines_list):
+    number_list = range(len(lines_list))
+    number_list = list(number_list)
+    for i in number_list:
+        num = random.randrange(3)
+        print(lines_list[num])
+# Testing working functions
+#yeezy_list = get_file_lines("poem.txt")
+#lines_printed_backwards(yeezy_list)
+#lines_list_printed_random(yeezy_list)
